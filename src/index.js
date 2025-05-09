f278cce369dd67dac92ca65b115d8a40f64726f9
AnhLove <tuankaito6005@gmail.com>
2025-05-09 21:00:26 +0700

    new

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const { engine } = require("express-handlebars");
 const port = 3000;
 
 const route = require("./routes");
+const db = require("./config/db");
+
+//Connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, "public")));
 
